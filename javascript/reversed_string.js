1d4293297e1111845143407dd453052b8d48d171
Camila Rangel <camilafariasrangel@gmail.com>
2021-11-10 06:57:34 -0300

    add: kata reversed string (8kyu)

---
+++ javascript/reversed_string.js
@@ -0,0 +1,11 @@
+// function solution(str){
+//     let splitStr = str.split('')
+//     let reverseStr = splitStr.reverse();
+//     let joinStr = reverseStr.join('')
+//     return joinStr
+// }
+    
+//Uma solução melhor:
+function solution(str){
+    return str.split('').reverse().join(''); 
+  }
